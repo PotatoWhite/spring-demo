694957453980222715e8c9cd027c3513e1d8647f
Potato White <bravopotato@gmail.com>
2018-11-29 02:19:31 +0900

    rename a package

---
+++ demo-oauth2/src/main/java/me/potato/demooauth2/user/User.java
@@ -0,0 +1,19 @@
+package me.potato.demooauth2.user;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class User {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String username;
+	private String password;
+}
---
+++ demo-oauth2/src/main/java/me/potato/demooauth2/user/UserService.java
@@ -0,0 +1,68 @@
+package me.potato.demooauth2.user;
+
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Log
+public class UserService implements UserDetailsService {
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+
+	public User save(User user) {
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+		return userRepository.save(user);
+	}
+
+	public void deletebyId(Long id) {
+		userRepository.deleteById(id);
+	}
+
+	@PostConstruct
+	public void init() {
+		Optional<User> byName = userRepository.findByUsername("potato");
+		if (!byName.isPresent()) {
+			User potato = new User();
+			potato.setUsername("potato");
+			potato.setPassword("potato123");
+
+			User saved = this.save(potato);
+			log.info(saved.toString());
+		}
+	}
+
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<User> byName = userRepository.findByUsername(username);
+		if (!byName.isPresent())
+			throw new UsernameNotFoundException(username);
+
+		User user = byName.get();
+		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), getAutorities());
+	}
+
+	private Collection<? extends GrantedAuthority> getAutorities() {
+		return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+}
